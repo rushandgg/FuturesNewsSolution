3fdc63eec739bd849a04d07026c31e6995c573ba
rushandgg <bso9419@naver.com>
2023-11-13 07:45:52 +0900

    First Commit, Test Prep API and Papago API

---
+++ FuturesNewsSol/MainForm.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace FuturesNewsSol
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.newsLoadBtn = new System.Windows.Forms.Button();
+            this.newsRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.contentRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.newsListView = new System.Windows.Forms.ListView();
+            this.title = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.date = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ticker = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.num = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contentRichTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // newsLoadBtn
+            // 
+            this.newsLoadBtn.Location = new System.Drawing.Point(329, 195);
+            this.newsLoadBtn.Name = "newsLoadBtn";
+            this.newsLoadBtn.Size = new System.Drawing.Size(175, 62);
+            this.newsLoadBtn.TabIndex = 1;
+            this.newsLoadBtn.Text = "NewsLoad";
+            this.newsLoadBtn.UseVisualStyleBackColor = true;
+            this.newsLoadBtn.Click += new System.EventHandler(this.newsLoadBtn_Click);
+            // 
+            // newsRichTextBox
+            // 
+            this.newsRichTextBox.Location = new System.Drawing.Point(12, 319);
+            this.newsRichTextBox.Name = "newsRichTextBox";
+            this.newsRichTextBox.Size = new System.Drawing.Size(414, 94);
+            this.newsRichTextBox.TabIndex = 3;
+            this.newsRichTextBox.Text = "";
+            // 
+            // contentRichTextBox
+            // 
+            this.contentRichTextBox.Location = new System.Drawing.Point(510, 30);
+            this.contentRichTextBox.Name = "contentRichTextBox";
+            this.contentRichTextBox.Size = new System.Drawing.Size(414, 290);
+            this.contentRichTextBox.TabIndex = 4;
+            this.contentRichTextBox.Text = "";
+            // 
+            // newsListView
+            // 
+            this.newsListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.num,
+            this.title,
+            this.date,
+            this.ticker});
+            this.newsListView.FullRowSelect = true;
+            this.newsListView.HideSelection = false;
+            this.newsListView.LabelWrap = false;
+            this.newsListView.Location = new System.Drawing.Point(12, 12);
+            this.newsListView.MultiSelect = false;
+            this.newsListView.Name = "newsListView";
+            this.newsListView.Size = new System.Drawing.Size(492, 177);
+            this.newsListView.TabIndex = 5;
+            this.newsListView.UseCompatibleStateImageBehavior = false;
+            this.newsListView.View = System.Windows.Forms.View.Details;
+            this.newsListView.SelectedIndexChanged += new System.EventHandler(this.newsListView_SelectedIndexChanged);
+            // 
+            // title
+            // 
+            this.title.Text = "Title";
+            this.title.Width = 200;
+            // 
+            // date
+            // 
+            this.date.Text = "Date";
+            this.date.Width = 120;
+            // 
+            // ticker
+            // 
+            this.ticker.Text = "Ticker";
+            this.ticker.Width = 120;
+            // 
+            // num
+            // 
+            this.num.Text = "Num";
+            this.num.Width = 40;
+            // 
+            // contentRichTextBox2
+            // 
+            this.contentRichTextBox2.Location = new System.Drawing.Point(510, 347);
+            this.contentRichTextBox2.Name = "contentRichTextBox2";
+            this.contentRichTextBox2.Size = new System.Drawing.Size(414, 290);
+            this.contentRichTextBox2.TabIndex = 6;
+            this.contentRichTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(510, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "원문";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(510, 329);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "번역";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 463);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 62);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "NewsLoad";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 680);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.contentRichTextBox2);
+            this.Controls.Add(this.newsListView);
+            this.Controls.Add(this.contentRichTextBox);
+            this.Controls.Add(this.newsRichTextBox);
+            this.Controls.Add(this.newsLoadBtn);
+            this.Name = "MainForm";
+            this.Text = "FuturesNewsSol";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button newsLoadBtn;
+        private System.Windows.Forms.RichTextBox newsRichTextBox;
+        private System.Windows.Forms.RichTextBox contentRichTextBox;
+        private System.Windows.Forms.ListView newsListView;
+        private System.Windows.Forms.ColumnHeader title;
+        private System.Windows.Forms.ColumnHeader date;
+        private System.Windows.Forms.ColumnHeader ticker;
+        private System.Windows.Forms.ColumnHeader num;
+        private System.Windows.Forms.RichTextBox contentRichTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ FuturesNewsSol/MainForm.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace FuturesNewsSol
+{
+    public partial class MainForm : Form
+    {
+        readonly static string apiKey = "";
+        List<string> contentList = new List<string>();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void newsLoadBtn_Click(object sender, EventArgs e)
+        {
+            // 뉴스 목록인 listview 초기화, 본문 목록인 list 초기화
+            newsListView.Items.Clear();
+            contentList.Clear();
+
+            int articlesSize = 2;
+            string fmpNewsUrl = $"https://financialmodelingprep.com/api/v3/fmp/articles?page=0&size={articlesSize}&apikey={apiKey}";
+
+            WebRequest webRequest;
+            webRequest = WebRequest.Create(fmpNewsUrl);
+
+            Stream objStream;
+            objStream = webRequest.GetResponse().GetResponseStream();
+            
+            StreamReader r = new StreamReader(objStream);
+            string text = r.ReadToEnd();
+            
+            // json 파싱, 불러온 수 만큼 content 인덱스 부여
+            JObject jObj = JObject.Parse(text);
+
+            // 전체 기사 개수만큼 반복
+            for (int i = 0; i < articlesSize; i++)
+            {
+                ListViewItem item = new ListViewItem($"{i+1}");
+                JToken article = jObj["content"][i];
+                string title = article.Value<string>("title");
+                string date = article.Value<string>("date");
+                string ticker = article.Value<string>("tickers");
+                string content = article.Value<string>("content");
+                content = Regex.Replace(content, "<[a-zA-Z/].*?>", String.Empty);
+                
+                item.SubItems.Add(title);
+                item.SubItems.Add(date);
+                item.SubItems.Add(ticker);
+                contentList.Add(content);
+
+                newsListView.Items.Add(item);
+            }
+        }
+
+        private void newsListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Null값일 때 오류 방지
+            if (newsListView.SelectedItems.Count != 0) 
+            { 
+                // 선택된 항목의 인덱스 가져옴
+                int SelectRow = newsListView.SelectedItems[0].Index;
+                // 제목 + 본문
+                contentRichTextBox.Text = newsListView.Items[SelectRow].SubItems[1].Text + "\n\n" + contentList[SelectRow].ToString();
+
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 번역문 만들기
+            string sUrl = "https://openapi.naver.com/v1/papago/n2mt";
+
+            // 파라미터에 값넣기 (파파고 NMT API 가이드에서 -d 부분이 파라미터이다)
+            //string sourceText = contentRichTextBox.Text;
+            
+            string sourceText = "GoodRx Shares Plunge 18aaaaaaaaaaaaaasssssssss%";
+            MessageBox.Show(sourceText);
+            
+            string sParam = string.Format($"source=en&target=ko&text={sourceText}");
+
+            // 파라미터를 Character Set에 맞게 변경
+            byte[] bytearry = Encoding.UTF8.GetBytes(sParam);
+            MessageBox.Show(bytearry.Length.ToString());
+            // 서버에 요청
+            WebRequest webRequest = WebRequest.Create(sUrl);
+            webRequest.Method = "POST";
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+
+            // 헤더 추가하기 (파파고 NMT API 가이드에서 -h 부분이 헤더이다)
+            webRequest.Headers.Add("X-Naver-Client-Id", "");
+            webRequest.Headers.Add("X-Naver-Client-Secret", "");
+
+            // 요청 데이터 길이
+            webRequest.ContentLength = bytearry.Length;
+
+            Stream stream = webRequest.GetRequestStream();
+            stream.Write(bytearry, 0, bytearry.Length);
+            stream.Close();
+
+            // 응답 데이터 가져오기(출력포맷)
+            WebResponse webResponse = webRequest.GetResponse();
+            stream = webResponse.GetResponseStream();
+            StreamReader streamReader = new StreamReader(stream);
+            string sReturn = streamReader.ReadToEnd();
+
+            streamReader.Close();
+            stream.Close();
+            webResponse.Close();
+
+            JObject jObject = JObject.Parse(sReturn);
+            // JSON 출력포맷에서 필요한 부분(번역된 문장)만 가져오기
+            contentRichTextBox2.Text = jObject["message"]["result"]["translatedText"].ToString();
+            
+        }
+    }
+}
